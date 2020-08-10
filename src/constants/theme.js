f0800ea0cab8ff09ee0e5e336870e2c5d55fc013
soeren.koerbelin <soeren.koerbelin@gmail.com>
2020-08-10 18:04:19 +0200

    Updates

--- src/constants/theme.js
+++
@@ -1,6 +0,0 @@
-export const accent = '#f90000';
-
-export const rgc_blue = '#041E42';
-export const rgc_red = '#F9423A';
-export const rgc_black = '#000000';
-export const rgc_lightgray = '#EDEDED';
